6c06e71077a36644275a9f0dcc3a0b9ffbb5ee16
zZBrigadierZz <109725229+zZBrigadierZz@users.noreply.github.com>
2022-10-10 14:32:06 +0200

    Added documentation to the time class.

--- src/main/java/com/github/engine/Time.java
+++ src/main/java/com/github/engine/Time.java
@@ -1,11 +1,18 @@
 package com.github.engine;
 
+/**
+* This class represents Time of the game and delta time on which
+* game is currently running.
+*/
 public final class Time {
 
     private static final long gameStart = System.currentTimeMillis();
     private static long deltaTime = 0;
     private static long time = System.nanoTime();
 
+    /**
+    * This method will update current time.
+    */
     public static void update() {
         System.nanoTime();
         deltaTime = System.nanoTime() - time;
